5c6646c2b560afebec583f10f535bf3c1d73afe6
Edbbioeco <edsonbbiologia@gmail.com>
2026-02-18 12:23:27 -0300

    Script para settar o repositório do GitHub

---
+++ settar_repositório.R
@@ -0,0 +1,26 @@
+# Pacote ----
+
+library(usethis)
+
+# Iniciando ----
+
+usethis::use_git()
+
+# Configure o usuario e email ----
+
+usethis::use_git_config(user.name = "Edbbioeco",
+                        user.email = "edsonbbiologia@gmail.com")
+
+# Checando o repositório ----
+
+usethis::proj_get()
+
+# Settando o repositório ----
+
+usethis::use_git_remote(name = "conectar_github",
+                        url = "https://github.com/Edbbioeco/conectar_github_rstudio.git",
+                        overwrite = TRUE)
+
+# Criando o branch main ----
+
+usethis::git_default_branch_configure()
